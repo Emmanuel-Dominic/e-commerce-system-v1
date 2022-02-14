b76ab3c3f1c331d77317ca10c6990ec8a2c48c0a
Matembu Emmanuel Dominic <ematembu2@gmail.com>
2022-02-14 12:19:40 +0000

    loads selenium test fixtures

--- config/tests/fixtures.py
+++ config/tests/fixtures.py
@@ -1,6 +1,8 @@
 import pytest
+from django.core.management import call_command
 
 
-@pytest.fixture
-def create_admin_user(django_user_model):
-    return django_user_model.objects.create_superuser("admin", "admin@admin.com", "password")
+@pytest.fixture(scope="session")
+def db_fixture_setup(django_db_setup, django_db_blocker):
+    with django_db_blocker.unblock():
+        call_command("loaddata", "db_admin_fixture.json")
